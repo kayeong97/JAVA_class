3c0555dbe265b228e7221d07d31343741ae72b8c
Ka Yeong Kim <165864773+kayeong97@users.noreply.github.com>
2024-09-23 22:33:20 +0900

    Create lab3-3.java

---
+++ 3장 실습/lab3-3.java
@@ -0,0 +1,17 @@
+public class Add {
+    public static void main(String[] args){
+        //예외처리 사용
+        //위에 벌레 옆에 ... 눌러서 제일 밑 눌러서 program arguments에 입력하면 됨
+
+        int sum = 0;
+        for(String s: args){
+            try { //예외처리 안 해주면 프로그램이 죽어버림
+                sum += Integer.parseInt(s);
+            }
+            catch(NumberFormatException e){
+                System.out.println(s+"는 정수가 아님");
+            }
+        }
+        System.out.println("합: "+ sum);
+    }
+}
